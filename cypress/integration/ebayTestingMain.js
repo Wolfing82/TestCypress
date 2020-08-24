2dfd0313235a52d43f3f23778fd63dde6a011389
Agustin Sala <salaa@avaya.com>
2020-08-24 15:52:35 -0300

    Minor fixes.

--- cypress/integration/ebayTestingMain.js
+++ cypress/integration/ebayTestingMain.js
@@ -1,38 +1,31 @@
 describe('Just Open ebay and perform a search', () => {
-	const urlBase = 'http://www.ebay.com'
 	const searchPilas =  'Pilas'
 	const SearchIdInput = '#gh-ac'
 	const searchIdButton = '#gh-btn'
 	
+	//load the main web site.
 	it('Open the base project', () => {
 	
-
-	cy.visit('/')
-    expect(true).to.equal(true)
-	
-	//cy.get('#gh-ac').type(urlSearch01)
-
-	
-	
-	cy.on('uncaught:exception', (err, runnable) => {
-		expect(err.message).to.include('This error is expected')
-	})
-	
-	//cy.get('.srp-controls__count-heading > :nth-child(1)')
-	
-	//cy.log('Number of search items found:', )
-	
+		cy.visit('/')
+		
+		cy.on('uncaught:exception', (err, runnable) => {
+			expect(err.message).to.include('This error is expected')
+		})
+		
 	})
   
+	//use the web site search engine.
 	it('Perform a custom search', () => {
 	  
 		cy.get(SearchIdInput).type(searchPilas)
 		cy.get(searchIdButton).click()
+		//todo: futher investigation. Appears to be a unhandled exception when using the search on the site(maybe the cookies???)
 		cy.on('uncaught:exception', (err, runnable) => {
 			expect(err.message).to.include('This error is expected')
 		})
 	})
 	
+	//Log the expected result.
 	it('check the results', () => {
 		cy.get('.srp-controls__count-heading > :nth-child(1)').then(($span) => {
 			const searchResults = $span.text();
